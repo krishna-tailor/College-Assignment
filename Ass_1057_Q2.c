8dfb68846f9e28135f0ddca3dd6b63202a3ab1d9
krishna-tailor <ks.tailor14@gmail.com>
2025-10-07 23:32:05 +0530

    Update Ass_1057_Q2.c

--- Ass_1057_Q2.c
+++ Ass_1057_Q2.c
@@ -4,9 +4,9 @@ int main() {
     printf("Enter three numbers: ");
    scanf("%d %d %d", &a, &b, &c);
    if(a<b && a<c)
-   printf("%d is the largest\n", a);
+   printf("%d is the smallest\n", a);
    else if(b<c && b<a)
-   printf("%d is the largest\n", b);
+   printf("%d is the smallest\n", b);
    else if (c<b && c<a)
-  printf("%d is the largest\n", c);
+  printf("%d is the smallest\n", c);
 }
